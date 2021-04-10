5857e6ecbbfcd239a47f9373c0387eb404a38cf2
Amelia <yzaguirre@dal.ca>
2021-04-09 22:49:10 -0700

    add color data

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Colors of the Moon
+#'
+#' A dataset containing 48 different colors of the Moon.
+#' Colors were extracted from photographs taken by Marcella Giulia Pace over a ten year period.
+#'
+#' @format A character vector containing 48 color hexcodes.
+#' @source \url{https://greenflash.photo/greenflash-gallery/greenflash-gallery/portfolio/colors-of-the-moon/}
+"moon_colors"
